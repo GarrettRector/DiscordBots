6dcc8094b10955fc7f66a25cffe07f02241518c5
Garrett Rector <garrett@grector.dev>
2021-10-16 22:45:33 -0400

    Update main.py

--- ripoff rythm/main.py
+++ ripoff rythm/main.py
@@ -2,17 +2,12 @@ import discord
 from discord.ext import commands
 import music
 
-client = commands.Bot(command_prefix="~", intents=discord.Intents.all())
-
 cogs = [music]
+
+client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
 
-client.run("KEY")
-
-
-@client.event
-async def on_ready():
-    print(f"{client.user} logged in")
+client.run("TOKEN")
